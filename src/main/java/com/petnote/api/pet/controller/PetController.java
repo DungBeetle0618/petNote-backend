084a6c1745a4dd0e1323af91410833f21c0bb706
wjdtjs <jungseon123@gmail.com>
2025-12-02 09:55:58 +0900

    refresh token 읽기 테스트

--- src/main/java/com/petnote/api/pet/controller/PetController.java
+++ src/main/java/com/petnote/api/pet/controller/PetController.java
@@ -1,7 +1,11 @@
 package com.petnote.api.pet.controller;
 
+import com.petnote.api.auth.jwt.JwtProvider;
 import com.petnote.api.pet.dto.PetDTO;
 import com.petnote.api.pet.service.PetService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -18,14 +22,29 @@ import java.util.Map;
 @RequestMapping("/api/pet")
 public class PetController {
     private final PetService petService;
+    private final JwtProvider jwtProvider;
 
     /**
      * 펫리스트 조회
      * @return
      */
     @GetMapping("/listForAxios")
-    public Map<String, Object> listForAxios() {
+    public Map<String, Object> listForAxios(@CookieValue(value = "refreshToken", required = false) String refreshToken, HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                System.out.println(cookie.getName() + " = " + cookie.getValue());
+            }
+        }
+
+
         String userId = "aaaa";
+        if(refreshToken != null){
+             userId = jwtProvider.parseToken(refreshToken).getBody().getSubject();
+            System.out.println("======> " +  userId);
+        } else {
+            System.out.println("======>리프레시 없음");
+        }
 
         Map<String, Object> resultMap = new HashMap<>();
         List<PetDTO> list = petService.selectList(userId);
@@ -46,6 +65,7 @@ public class PetController {
         String userId = "aaaa";
         petDTO.setUserId(userId);
 
+        //TODO: 첫 동물이면 자동으로 대표 설정.
         petService.insertPet(petDTO);
 
         Map<String, Object> resultMap = new HashMap<>();
